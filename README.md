49c7a99bdfd60c0b026c325f6c41a071d5523429
safwanshoaib <safwanshoaib2002@gmail.com>
2024-07-21 21:23:50 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Jetpack Compose 📱
+
+Just creating a Greeting Activity which contain information about profile Image , Full Name and CGPA
+
+
+## Screenshots
+
+![App Screenshot]("")
+
+
+## Screenshots
+
+![App Screenshot](app/src/whatsapp_image.png)
+
+
+## Authors
+
+- [@safwanshoaib](https://github.com/safwanshoaib)
+
---
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Image(painter = painterResource(id = R.drawable.profile_img), contentDescription = "Profile_img")
+            Text(
+                text = "Name:   Safwan Shoaib",
+                modifier = modifier,
+                color = MaterialTheme.colorScheme.secondary,
+                fontSize = 18.sp
+            )
+            Text(
+                text = "Class:   BSCS",
+                modifier = modifier,
+                color = MaterialTheme.colorScheme.secondary,
+                fontSize = 18.sp
+            )
+            Text(
+                text = "CGPA:   2.87",
+                modifier = modifier,
+                color = MaterialTheme.colorScheme.secondary,
+                fontSize = 18.sp
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeTheme {
+        Greeting("Android")
+    }
+}
